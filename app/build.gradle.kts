77c858f1864ed9623c9f369a57d6a247ba38a76e
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2023-10-15 00:23:49 +0800

    create initial layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.mobdeve.chuachingdytocstamaria.mco.pomopet"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mobdeve.chuachingdytocstamaria.mco.pomopet"
