81e28d26feecf66d5d1032273bbe711d43e16a5d
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2023-10-16 16:45:33 +0800

    add timer functions

--- app/src/main/java/com/mobdeve/chuachingdytocstamaria/mco/pomopet/MainActivity.kt
+++ app/src/main/java/com/mobdeve/chuachingdytocstamaria/mco/pomopet/MainActivity.kt
@@ -2,6 +2,8 @@ package com.mobdeve.chuachingdytocstamaria.mco.pomopet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.chuachingdytocstamaria.mco.pomopet.adapters.TodoAdapter
@@ -11,6 +13,14 @@ import com.mobdeve.chuachingdytocstamaria.mco.pomopet.models.ToDo
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var todoListRV: RecyclerView
+
+    private lateinit var countdownTimer: CountDownTimer
+
+    private var isRunning = false
+
+    private var initialTimeInMins = 25
+    var timeInMs = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,5 +32,54 @@ class MainActivity : AppCompatActivity() {
             LinearLayoutManager.VERTICAL,
             false)
 
+        binding.startBtn.setOnClickListener{
+            val time = initialTimeInMins * 60000L
+            startTimer(time)
+        }
+
+        binding.pauseResumeBtn.setOnClickListener{
+            if(!isRunning){
+                startTimer(timeInMs)
+            } else{
+                pauseTimer()
+
+            }
+        }
+
     }
+
+    private fun pauseTimer(){
+        binding.pauseResumeBtn.text = "Resume"
+        countdownTimer.cancel()
+        isRunning = false
+
+    }
+
+    private fun startTimer(time: Long){
+        countdownTimer = object : CountDownTimer(time, 1000){
+            override fun onFinish() {
+                TODO("Handle changing between breaks and increasing cycles")
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                timeInMs = millisUntilFinished
+                updateText()
+            }
+        }
+
+        binding.startBtn.visibility = View.INVISIBLE
+        binding.timerControlGroupLL.visibility = View.VISIBLE
+        binding.pauseResumeBtn.text = "Pause"
+        countdownTimer.start()
+        isRunning = true
+
+    }
+
+    private fun updateText(){
+        val minute = (timeInMs / 1000) / 60
+        val seconds = (timeInMs / 1000) % 60
+
+        binding.timerTV.text = "$minute:$seconds"
+    }
+
 }
