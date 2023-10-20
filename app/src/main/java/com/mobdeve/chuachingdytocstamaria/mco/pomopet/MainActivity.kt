cef143e1c69f662a6d63a06ab3204ae0e5c0a50c
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2023-10-21 02:19:50 +0800

    add preference reflection

--- app/src/main/java/com/mobdeve/chuachingdytocstamaria/mco/pomopet/MainActivity.kt
+++ app/src/main/java/com/mobdeve/chuachingdytocstamaria/mco/pomopet/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.chuachingdytocstamaria.mco.pomopet.adapters.TodoAdapter
@@ -14,13 +17,32 @@ import com.mobdeve.chuachingdytocstamaria.mco.pomopet.models.ToDo
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var todoListRV: RecyclerView
-
     private lateinit var countdownTimer: CountDownTimer
-
     private var isRunning = false
 
-    private var initialTimeInMins = 20
+    private var initialTimeInMins = SettingsActivity.DEFAULT_POMODORO_TIME
+    private var shortBreakTimeInMins = SettingsActivity.DEFAULT_SHORT_BREAK
+    private var longBreakTimeInMins = SettingsActivity.DEFAULT_LONG_BREAK
     var timeInMs = initialTimeInMins * 60000L
+
+    private val settingsLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode == RESULT_OK) {
+            val pomodoroTime = result.data?.getIntExtra(SettingsActivity.POMODORO_TIME_KEY,
+                SettingsActivity.DEFAULT_POMODORO_TIME)!!
+
+            this.shortBreakTimeInMins = result.data?.getIntExtra(SettingsActivity.SHORT_BREAK_KEY,
+                SettingsActivity.DEFAULT_SHORT_BREAK)!!
+
+            this.longBreakTimeInMins = result.data?.getIntExtra(SettingsActivity.LONG_BREAK_KEY,
+                SettingsActivity.DEFAULT_LONG_BREAK)!!
+
+            this.initialTimeInMins = pomodoroTime
+            this.timeInMs = minsToMs(pomodoroTime)
+
+            updateText()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +58,7 @@ class MainActivity : AppCompatActivity() {
         updateText()
 
         binding.startBtn.setOnClickListener{
-            val time = initialTimeInMins * 60000L
+            val time = minsToMs(initialTimeInMins)
             startTimer(time)
             toggleViewElements(View.INVISIBLE)
         }
@@ -59,8 +81,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.settingsBtn.setOnClickListener{
             val intent = Intent(binding.root.context, SettingsActivity::class.java)
+            intent.putExtra(SettingsActivity.POMODORO_TIME_KEY, initialTimeInMins)
+            intent.putExtra(SettingsActivity.SHORT_BREAK_KEY, shortBreakTimeInMins)
+            intent.putExtra(SettingsActivity.LONG_BREAK_KEY, longBreakTimeInMins)
 
-            startActivity(intent)
+            settingsLauncher.launch(intent)
         }
 
         binding.streakBtn.setOnClickListener{
@@ -70,6 +95,10 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    private fun minsToMs(timeInMins: Int): Long{
+        return timeInMins * 60000L
     }
 
     private fun toggleViewElements(visibility: Int){
--- app/src/main/java/com/mobdeve/chuachingdytocstamaria/mco/pomopet/SettingsActivity.kt
+++ app/src/main/java/com/mobdeve/chuachingdytocstamaria/mco/pomopet/SettingsActivity.kt
@@ -3,21 +3,134 @@ package com.mobdeve.chuachingdytocstamaria.mco.pomopet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mobdeve.chuachingdytocstamaria.mco.pomopet.databinding.ActivityMainBinding
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.mobdeve.chuachingdytocstamaria.mco.pomopet.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+    companion object{
+        const val POMODORO_TIME_KEY = "POMODORO_TIME_KEY"
+        const val SHORT_BREAK_KEY = "SHORT_BREAK_KEY"
+        const val LONG_BREAK_KEY = "LONG_BREAK_KEY"
+        const val DEFAULT_POMODORO_TIME = 25
+        const val DEFAULT_SHORT_BREAK = 5
+        const val DEFAULT_LONG_BREAK = 10
+    }
+
+
     private lateinit var binding: ActivitySettingsBinding
+    private var isSaved = false
+    private var pomodoroTime: Int = SettingsActivity.DEFAULT_POMODORO_TIME
+    private var shortBreakTime: Int = SettingsActivity.DEFAULT_SHORT_BREAK
+    private var longBreakTime: Int = SettingsActivity.DEFAULT_LONG_BREAK
+    private lateinit var pomodoroETNumber: EditText
+    private lateinit var longBreakETNumber: EditText
+    private lateinit var shortBreakETNumber: EditText
+    private lateinit var saveBtn: Button
+    private lateinit var defaultBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        pomodoroETNumber = binding.pomodoroETNumber
+        longBreakETNumber = binding.longBreakETNumber
+        shortBreakETNumber = binding.shortBreakETNumber
+        saveBtn = binding.saveBtn
+        defaultBtn = binding.defaultBtn
+
+
+
+        pomodoroTime = intent.getIntExtra(SettingsActivity.POMODORO_TIME_KEY,
+            SettingsActivity.DEFAULT_POMODORO_TIME)
+
+        shortBreakTime = intent.getIntExtra(SettingsActivity.SHORT_BREAK_KEY,
+            SettingsActivity.DEFAULT_SHORT_BREAK)
+
+        longBreakTime = intent.getIntExtra(SettingsActivity.LONG_BREAK_KEY,
+            SettingsActivity.DEFAULT_LONG_BREAK)
+
+        pomodoroETNumber.setText(pomodoroTime.toString())
+        shortBreakETNumber.setText(shortBreakTime.toString())
+        longBreakETNumber.setText(longBreakTime.toString())
 
         binding.settingsBackBtn.setOnClickListener{
+            if(isSaved){
+                val returnIntent = Intent()
+                returnIntent.putExtra(SettingsActivity.POMODORO_TIME_KEY,
+                    pomodoroETNumber.text.toString().toInt())
+                returnIntent.putExtra(SettingsActivity.SHORT_BREAK_KEY,
+                    shortBreakETNumber.text.toString().toInt())
+                returnIntent.putExtra(SettingsActivity.LONG_BREAK_KEY,
+                    longBreakETNumber.text.toString().toInt())
+                setResult(RESULT_OK, returnIntent)
+            }
             finish()
         }
 
+        saveBtn.setOnClickListener{
+            this.isSaved = true
+            Toast.makeText(this, "Settings saved", Toast.LENGTH_LONG).show()
+            saveBtn.isEnabled = false
+            saveBtn.isClickable = false
+        }
+
+        defaultBtn.setOnClickListener{
+            this.isSaved = false
+            saveBtn.isEnabled = true
+            saveBtn.isClickable = true
+            setDefaults()
+        }
+
+        val textWatcher: TextWatcher = object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                isSaved = false
+                if(isTextStillOriginal().not()){
+                    Log.d("on change", "changed not original")
+                    saveBtn.isEnabled = true
+                    saveBtn.isClickable = true
+                } else {
+                    Log.d("on change", "changed original")
+                    Log.d("on change", "$pomodoroTime")
+                    saveBtn.isEnabled = false
+                    saveBtn.isClickable = false
+                }
+            }
+        }
+
+        longBreakETNumber.addTextChangedListener(textWatcher)
+        shortBreakETNumber.addTextChangedListener(textWatcher)
+        pomodoroETNumber.addTextChangedListener(textWatcher)
+
+    }
+
+    private fun isTextStillOriginal() : Boolean {
+        if((this.pomodoroETNumber.text.toString().isEmpty()) or
+            (this.shortBreakETNumber.text.toString().isEmpty())or
+            (this.longBreakETNumber.text.toString().isEmpty())) {
+            return true
+        }
+
+        return (this.pomodoroETNumber.text?.toString()?.toInt() == pomodoroTime) and
+                (this.shortBreakETNumber.text?.toString()?.toInt() == shortBreakTime) and
+                (this.longBreakETNumber.text?.toString()?.toInt() == longBreakTime)
+    }
+
+    private fun setDefaults(){
+        pomodoroETNumber.setText(SettingsActivity.DEFAULT_POMODORO_TIME.toString())
+        shortBreakETNumber.setText(SettingsActivity.DEFAULT_SHORT_BREAK.toString())
+        longBreakETNumber.setText(SettingsActivity.DEFAULT_LONG_BREAK.toString())
 
     }
 }
